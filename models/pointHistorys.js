6fe3ab925ec4db97e060983d8038877f60bfa9f7
dongil Kim <kdi@onlykcs.com>
2019-05-29 09:42:44 +0900

    - type 추가

--- models/pointHistorys.js
+++ models/pointHistorys.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pointHistorysSchema = new Schema({
+    type: String,
     extType: String,
     pointId: String,
     category: String,          
