df9a98683c1a8e712cb19ac5e3ff7bbe710edbe9
yeongcoder <tlsqnsla15@naver.com>
2019-06-04 14:09:28 +0900

    [퍼블리싱] 게임스테이션 css작업중

--- routes/gameStation.js
+++ routes/gameStation.js
@@ -26,9 +26,25 @@ router.get('/trade/assets/', function (req, res, next) {
     }
 });
 
+router.get('/trade/exchange_assets/', function (req, res, next) {
+    if(dbconfig.country == "KR") {
+        res.render('v2/gamestation/exchange_gameassets', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
+    } else {
+        //res.render('v2_en/howto/tradevtr', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
+    }
+});
+
 router.get('/trade/mach/', function (req, res, next) {
     if(dbconfig.country == "KR") {
         res.render('v2/gamestation/machtogameassets', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
+    } else {
+        //res.render('v2_en/howto/tradevtr', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
+    }
+});
+
+router.get('/trade/exchange_mach/', function (req, res, next) {
+    if(dbconfig.country == "KR") {
+        res.render('v2/gamestation/exchange_mach', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
     } else {
         //res.render('v2_en/howto/tradevtr', {title: 'Bitweb Main', userId: req.session.userId, coinId: req.session.coinId, authPhone: req.session.authPhone, pointId: req.session.pointId, useBlockchain:dbconfig.useBlockchain, usePoint:dbconfig.usePoint});
     }
