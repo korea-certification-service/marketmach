1be417488dc8b4705fc90c354b80c6038330a330
yeongcoder <tlsqnsla15@naver.com>
2019-06-11 18:34:26 +0900

    마이페이지 숫자인풋태그 글자수제한

--- public/static/js/setAttr.js
+++ public/static/js/setAttr.js
@@ -1,21 +1,30 @@
 window.addEventListener("load", function(){
     var price = document.querySelector("#price");
     var point = document.querySelector("#point");
+    var amount = document.querySelector("#amount");
 
-    price.setAttribute("maxLength", "10");
-    point.setAttribute("maxLength", "14");
-
-    price.addEventListener("input", function(){
-        maxLengthCheck(price)
-        console.log(1);
-    });
-    point.addEventListener("input", function(){
-        maxLengthCheck(point)
-    });
+    if(price != null){
+        price.setAttribute("maxLength", "10");
+        price.addEventListener("input", function(){
+            maxLengthCheck(price)
+        });
+    }
+    if(point != null){
+        point.setAttribute("maxLength", "14");
+        point.addEventListener("input", function(){
+            maxLengthCheck(point)
+        });
+    }
+    if(amount != null){
+        amount.setAttribute("maxLength", "14");
+        amount.addEventListener("input", function(){
+            maxLengthCheck(amount)
+        });
+    }
 
     function maxLengthCheck(object){
         if (object.value.length > object.maxLength){
-          object.value = object.value.slice(0, object.maxLength);
+            object.value = object.value.slice(0, object.maxLength);
         }    
-      }
+    }
 });
