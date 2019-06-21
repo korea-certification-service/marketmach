6bce6e8dbcd61d70041f7319947452cf4669e81d
dongil Kim <kdi@onlykcs.com>
2019-06-21 19:01:19 +0900

    - 파라미터 값 추가

--- routes/points.js
+++ routes/points.js
@@ -517,10 +517,10 @@ router.post('/happymoney/pin/payment', function(req, res, next) {
         "QueryGiftQty":1,
         "PinNo":cryptojs.TripleDES.encrypt("5746606380005303",dbconfig.happymoney.onlineId),
         "PinDate":cryptojs.TripleDES.encrypt("20190422",dbconfig.happymoney.onlineId),
-        "BranchCode": "",
+        "BranchCode": "0",
         "SalesDate": today.substr(0,7),
-        "PosNo": "",
-        "InputKind":""
+        "PosNo": "0",
+        "InputKind":"0"
 
     }
     //상품권 조회
