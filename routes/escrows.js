ca3ef40402560c5ae1f1338d514d8bc29cf98a92
dongil Kim <kdi@onlykcs.com>
2019-08-01 21:11:50 +0900

    - 상태값 추가

--- routes/escrows.js
+++ routes/escrows.js
@@ -55,6 +55,7 @@ router.get('/:userId/total_escrow/:coinType', function (req, res, next) {
     let coinType = req.params.coinType;
     let condition = {
         $or: [{"from_userId": userId}, {"to_userId": userId}],
+        "item.status":[2,3],
         "completed": {$exists: false}
     }
     var bitwebResponse = new BitwebResponse();
