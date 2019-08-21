151b7266a7add5b013055cba9545c64669e3bcbe
Reinkaos <ds2lvg@gmail.com>
2019-08-21 15:24:19 +0900

    [커뮤니티] 한글, 영문 댓글 제대로 표시되게 완성

--- public/static/js/util_en.js
+++ public/static/js/util_en.js
@@ -432,3 +432,18 @@ function addClassCommunityLeftMenu(type) {
         if(type === href.match(/[\w]*$/g)[0]) $li.eq(i).addClass("on");
     }
 }
+
+function ajaxLoginYnCheck(callback) {
+    $.ajax({
+        method: "GET",
+        url: "/v1/ajaxLoginYnCheck"        
+    }).done(function (success) {
+        if(success.data.loginYn == "Y") {
+            callback();
+        } else {
+            location.replace('/login');
+        }
+    }).fail(function (fail) {
+        location.replace('/login');
+    })
+}
