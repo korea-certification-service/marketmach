ef3cea6823d9be4a01043fe4873c830482451e52
reinkaos <ds2lvg@gmail.com>
2019-10-02 10:12:14 +0900

    [포인트몰] 메인에 마하어드벤처 다운로드 링크 모바일 구분해서 적용

--- public/static/js/util_en.js
+++ public/static/js/util_en.js
@@ -24,9 +24,18 @@ function isIos() {
     var tempUser = navigator.userAgent;
     var isMobile = false;
 
-    // userAgent 값에 iPhone, iPad, ipot, Android 라는 문자열이 하나라도 존재한다면 모바일로 간주됨.
     if (tempUser.indexOf("iPhone") > 0 || tempUser.indexOf("iPad") > 0
         || tempUser.indexOf("iPot") > 0) {
+        isMobile = true;
+    }
+    return isMobile;
+};
+
+function isAndroid() {
+    var tempUser = navigator.userAgent;
+    var isMobile = false;
+
+    if (tempUser.indexOf("Android") > 0) {
         isMobile = true;
     }
     return isMobile;
