0a6a7274d2aa5683d70389f07b0b042d8866ac67
cho-ks <ghotjfm@gmail.com>
2020-09-23 20:31:29 +0900

    오류 수정

--- libs/banner_event.js
+++ libs/banner_event.js
@@ -2,4 +2,4 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var banner_eventSchema = require('../models/banner_eventSchema');
 
-module.exports = mongoose.model('BannerEvent', banner_eventSchema);
+module.exports = mongoose.model('banner_event', banner_eventSchema);
--- models/banner_event.js
+++ models/banner_event.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var banner_eventSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     banner_name: String,
     banner_desc: String,
     banner_img_pc: String,
