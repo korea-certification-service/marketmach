fa41d855902ad030096135aaead1319138d0e16c
cho-ks <ghotjfm@gmail.com>
2020-09-23 20:33:12 +0900

    오타 수정

--- libs/banner_event.js
+++ libs/banner_event.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var banner_eventSchema = require('../models/banner_eventSchema');
+var banner_eventSchema = require('../models/banner_event');
 
 module.exports = mongoose.model('banner_event', banner_eventSchema);
